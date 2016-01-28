4cb8efa67062ff6e7609b60476155240e6becb59
xabiko <jdejprado@gmail.com>
2016-01-27 19:44:57 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,18 @@
+#Getting and cleaning data Course Project
+This project contains a script that applies certain transformations to [this dataset](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones).
+
+##Content:
+1. run_analysis.R : refer to CodeBook for setup and description.
+2. CodeBook.md : setup and description.
+3. README.md
+
+##Required libraries:
+* dplyr
+
+##Assumptions:
+* It is assumed that everyone who tests the script for evaluation purposes has downloaded [this dataset](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip), and has unzipped it completely.
+* It is assumed that the unzipped data keeps the same file-structure as the zipped data.
+
+##Acknowledgements:
+Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. A Public Domain Dataset for Human Activity Recognition Using Smartphones. 21th European Symposium on Artificial Neural Networks, Computational Intelligence and Machine Learning, ESANN 2013. Bruges, Belgium 24-26 April 2013.
+
---
+++ run_analysis.R
@@ -0,0 +1,70 @@
+run_analysis <- function(dataDir){
+        
+        # Loading dplyr & setting working directory (UCI HAR Dataset)
+        
+        library(dplyr)
+        setwd(dataDir)
+        
+        # Reading tables
+        
+        subject_train   <- read.table("train/subject_train.txt",col.names = "SUBJECT")
+        y_train         <- read.table("train/y_train.txt",col.names = "ACTIVITY")
+        X_train         <- read.table("train/X_train.txt")
+        subject_test    <- read.table("test/subject_test.txt",col.names = "SUBJECT")
+        y_test          <- read.table("test/y_test.txt",col.names = "ACTIVITY")
+        X_test          <- read.table("test/X_test.txt")
+        features        <- read.table("features.txt")
+        activity_labels <- read.table("activity_labels.txt")
+        
+        # STEP 1: Binding raw data and naming it "COMPLETE"
+        
+        TRAIN                   <- cbind( subject_train, y_train, X_train )
+        TEST                    <- cbind( subject_test , y_test , X_test )
+        COMPLETE                <- rbind( TRAIN , TEST )
+        
+        # Building vector with filtered variables
+        
+        means                   <- grep( "[Mm]ean" , features$V2)
+        stds                    <- grep( "std" , features$V2)
+        positionFeatures        <- sort(c(means,stds))
+        filteredFeatures        <- sub( "" , "V" , positionFeatures)
+        filteredFeatures        <- c("SUBJECT","ACTIVITY",filteredFeatures)
+        
+        # STEP 2: Selecting filtered variables from COMPLETE table
+        
+        COMPLETE        <- select_(COMPLETE,.dots = filteredFeatures)
+        
+        # STEP 3: Naming ACTIVITY variable values       
+        
+        COMPLETE$ACTIVITY <- merge(COMPLETE$ACTIVITY, activity_labels, by=1)[,2]
+        
+        # STEP 4: Re-labeling variables
+        
+        namesFeatures           <- as.character(features[positionFeatures,2])
+        namesFeatures           <- sub("^t","time", namesFeatures)
+        namesFeatures           <- sub("^f","freq", namesFeatures)
+        namesFeatures           <- gsub("-",".", namesFeatures)
+        namesFeatures           <- sub("\\()","",namesFeatures)
+        namesFeatures           <- c("SUBJECT","ACTIVITY",namesFeatures)
+        
+        colnames(COMPLETE)      <- namesFeatures
+        
+        # Final arrange for COMPLETE table
+        
+        COMPLETE        <- arrange(COMPLETE,SUBJECT,ACTIVITY)
+        
+        # STEP 5: creating COMPLETE.mean table
+        
+        COMPLETE.mean           <- aggregate(COMPLETE[,3:88], by=list(x$SUBJECT,x$ACTIVITY), FUN="mean")
+        colnames(COMPLETE.mean) <- namesFeatures
+        COMPLETE.mean           <- arrange(COMPLETE.mean,SUBJECT)
+        
+        # Showing results
+        
+        View(COMPLETE)
+        View(COMPLETE.mean)
+        
+        #return(COMPLETE)
+        #return(COMPLETE.mean)
+        
+}
